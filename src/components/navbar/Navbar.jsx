08946984f0c691d0fb9435cc55d7bb80b3f350ef
Tomek <tdzierzega@itppolska.pl>
2025-01-25 22:55:07 +0100

    PIERWSZY COMMIT

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>LamaEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/" className="register">
+          Sign up
+        </a>
+        <div className="menuIcon">
+          <img
+            src="/public/menu.png"
+            alt=""
+            onClick={() => setOpen(!open)}
+            //onClick={() => setOpen((prev) => !prev)} //to samo co wyzej tylko z uzyciem wartosci prev
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

77245e13e8c2c79f29a889df19df12adafc2b805
Tomek <tdzierzega@itppolska.pl>
2025-02-05 12:11:49 +0100

    Frontend end - part 1.

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./chat.scss";
+
+const Chat = () => {
+  const [chat, setChat] = useState(true);
+  return (
+    <div className="chat">
+      <div className="messages">
+        <h1>Messages</h1>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>John Doe</span>
+          <p>Lorem ipsum.....</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>John Doe</span>
+          <p>Lorem ipsum.....</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>John Doe</span>
+          <p>Lorem ipsum.....</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>John Doe</span>
+          <p>Lorem ipsum.....</p>
+        </div>
+      </div>
+      {chat && (
+        <div className="chatBox">
+          <div className="top">
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+              John Doe
+            </div>
+            <span className="close" onClick={() => setChat(null)}>
+              X
+            </span>
+          </div>
+          <div className="center">
+            <div className="chatMessage">
+              <p>Loremdkfjdjfldkjflkdjflkfd</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Loremdkfjdjfldkjflkdjflkfd</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Loremdkfjdjfldkjflkdjflkfd</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Loremdkfjdjfldkjflkdjflkfd</p>
+              <span>1 hour ago</span>
+            </div>
+          </div>
+          <div className="bottom">
+            <textarea name="" id=""></textarea>
+            <button>Send</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Chat;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-  const user = false;
+  const user = true;
 
   return (
     <nav>
@@ -19,7 +20,17 @@ const Navbar = () => {
       </div>
       <div className="right">
         {user ? (
-          <div></div>
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <span>John Doe</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
         ) : (
           <>
             <a href="/">Sign in</a>
